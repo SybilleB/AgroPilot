b71c70295330d64ff32a351ba2275158c06d6d94
an-rbd <angierobergeaud01@gmail.com>
2026-04-27 22:21:12 +0200

    modification Api.ts

--- React/constants/Api.ts
+++ React/constants/Api.ts
@@ -1,7 +1,7 @@
 import { Platform } from 'react-native';
 
 function resolveApiUrl(): string {
-  return 'http://10.111.1.13:8000/'; // Remplace par l'IP de ton serveur local (carte réseau de ton PC sur le meme réseau que ton téléphone)
+  return 'http://192.168.1.16:8000'; // Remplace par l'IP de ton serveur local (carte réseau de ton PC sur le meme réseau que ton téléphone)
 }
 
 export const API_BASE_URL = resolveApiUrl();
