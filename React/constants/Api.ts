f310951de606fdf7c6cb0552c6466900e6fab6e3
an-rbd <angierobergeaud01@gmail.com>
2026-04-27 21:58:34 +0200

    1er commit branch angie

--- React/constants/Api.ts
+++ React/constants/Api.ts
@@ -1,19 +1,19 @@
-import { Platform } from "react-native";
+import { Platform } from 'react-native';
 
 function resolveApiUrl(): string {
-  return "http://10.111.1.32:8000/"; // Remplace par l'IP de ton serveur local (carte réseau de ton PC sur le meme réseau que ton téléphone)
+  return 'http://10.111.1.13:8000/'; // Remplace par l'IP de ton serveur local (carte réseau de ton PC sur le meme réseau que ton téléphone)
 }
 
 export const API_BASE_URL = resolveApiUrl();
 
 export const API_ROUTES = {
   rentabilite: {
-    simulate: "/rentabilite/simulate",
+    simulate: '/rentabilite/simulate',
   },
   subventions: {
-    suggestions: "/subventions/suggestions",
+    suggestions: '/subventions/suggestions',
   },
   marches: {
-    prix: "/marches/prix",
+    prix: '/marches/prix',
   },
 } as const;
